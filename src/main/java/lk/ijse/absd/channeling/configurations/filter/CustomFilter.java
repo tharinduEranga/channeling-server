77833e23e9089bd1943fda55ccbbe044cfae20b7
tharindu-e <tharindue@ceyentra.com>
2019-09-10 20:28:23 +0530

    added paths to be ignored in token filter and ignored OPTIONS method

--- src/main/java/lk/ijse/absd/channeling/configurations/filter/CustomFilter.java
+++ src/main/java/lk/ijse/absd/channeling/configurations/filter/CustomFilter.java
@@ -28,15 +28,25 @@ public class CustomFilter implements Filter {
 
         String authorization = httpServletRequest.getHeader("authorization");
         String servletPath = httpServletRequest.getServletPath();
-        LOGGER.info("\nPath: " + servletPath + "\ntoken: " + authorization);
+        String httpMethod = httpServletRequest.getMethod();
+        LOGGER.info("\nPath: " + servletPath + "\ntoken: " + authorization + "\nmethod: " + httpMethod);
+
+        if (httpMethod.equalsIgnoreCase("OPTIONS")) {
+            filterChain.doFilter(httpServletRequest, httpServletResponse);
+            return;
+        }
+
+        boolean hasToIgnore = ignorePaths(httpServletRequest, httpServletResponse, filterChain, servletPath,
+                "/admins/login", "/index.html");
+        if (hasToIgnore) {
+            filterChain.doFilter(httpServletRequest, httpServletResponse);
+            return;
+        }
 
         try {
             Claims claims = JWTAuthenticator.decodeJWT(authorization);
         } catch (Exception e) {
-            if (servletPath.equalsIgnoreCase("/admins/login")) {
-                filterChain.doFilter(httpServletRequest, httpServletResponse);
-                return;
-            }
+
             e.printStackTrace();
             httpServletResponse.setStatus(401);
             httpServletResponse.sendError(401, "Invalid token!");
@@ -49,6 +59,27 @@ public class CustomFilter implements Filter {
 
     @Override
     public void destroy() {
+    }
+
+    /**
+     * @param httpServletRequest:  The container generated servlet request object which sends from client
+     * @param httpServletResponse: The response that will be generated from the server
+     * @param filterChain:         the filterChain of the request
+     * @param servletPath:         the servlet rest controller path of the request
+     * @param ignorePaths:         the paths which needs to be ignored for token authorization
+     * @return whether the path is equals to be ignored or not
+     * @throws IOException
+     * @throws ServletException
+     */
+    private boolean ignorePaths(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                                FilterChain filterChain, String servletPath, String... ignorePaths) throws IOException, ServletException {
+        String[] ignorePathsArray = ignorePaths.clone();
+        for (String ignorePath : ignorePathsArray) {
+            if (ignorePath.equalsIgnoreCase(servletPath)) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
